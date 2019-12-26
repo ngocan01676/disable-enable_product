9e23226ee78ef1d08d6214afb3945c3dbc64cd9c
an <test@gmail.com>
2019-12-26 13:47:27 +0700

    test

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'AN_QtyEvent',
+    'AN_OrderHistory',
     __DIR__
 );
